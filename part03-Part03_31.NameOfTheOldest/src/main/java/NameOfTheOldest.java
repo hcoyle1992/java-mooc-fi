b49025c198473659fb1ba30c24c60450a1381b0e
Harry <hcoyle1992@gmail.com>
2026-05-07 22:21:25 +0100

    Ignore build files

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NameOfTheOldest {
@@ -6,6 +7,25 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int oldestAge = 0;
+        String oldestName = "";
+        
+        while (true) {
+            String person = scanner.nextLine();
+            
+            if (person.equals("")) {
+                break;
+            }
+            
+            String[] nameAndAge = person.split(",");
+            String name = nameAndAge[0];
+            int age = Integer.parseInt(nameAndAge[1]);
+            
+            if (age > oldestAge) {
+                oldestName = name;
+            }
+        }
+        
+        System.out.println("Name of the oldest: " + oldestName);
     }
 }
