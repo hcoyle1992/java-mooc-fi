859f9afdac22bab7cad1e20870f8607180283911
Harry <hcoyle1992@gmail.com>
2026-05-07 23:00:45 +0100

    - create account with a balance - deposit and print new balance
     (website says use .balance() but it's private and inaccessible so used .saldo() to return
    -

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,15 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        Account artosAccount = new Account("Arto's account", 100.00);
+
+        System.out.println("Initial state");
+        System.out.println(artosAccount);
+
+        artosAccount.deposit(20);
+        System.out.println("The balance of Arto's account is now: " + artosAccount.saldo());
+
+        System.out.println("End state");
+        System.out.println(artosAccount);
     }
 }
