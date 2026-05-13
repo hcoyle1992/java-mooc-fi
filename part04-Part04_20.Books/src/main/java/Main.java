eaecb5b3a9c61d3220a524935e494c0f9888dc81
Harry <hcoyle1992@gmail.com>
2026-05-13 20:24:25 +0100

    create a program that reads book info from the user then asks user what to print - title, pages, publication year - empty string as the name stops the program - new class Book with constructor, getter and toString override

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -4,9 +4,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
 
+        while (true) {
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+
+            if (title.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Pages: ");
+            int pages = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("Publication year: ");
+            int year = Integer.parseInt(scanner.nextLine());
+
+            books.add(new Book(title, pages, year));
+        }
+
+        System.out.println();
+        System.out.print("What information will be printed? ");
+        String printWhat = scanner.nextLine();
+
+        for (Book book : books) {
+            if (printWhat.equalsIgnoreCase("everything")) {
+                System.out.println(book.toString());
+            } else if (printWhat.equalsIgnoreCase("name")) {
+                System.out.println(book.getTitle());
+            }
+        }
     }
 }
