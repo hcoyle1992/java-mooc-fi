c8afbda50487c0c2334dd3bd0c91a433e701002c
Harry <hcoyle1992@gmail.com>
2026-05-13 19:48:14 +0100

    Passing multiple parameters to object constructors and printing them on their own line.

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,30 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+
+        while (true) {
+            System.out.print("First name: ");
+            String fName = scanner.nextLine();
+
+            // just one break as this is how the exercise is tested.
+            if (fName.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Last name: ");
+            String lName = scanner.nextLine();
+
+            System.out.print("Identification number: ");
+            String id = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(fName, lName, id));
+
+        }
+
+        System.out.println();
+
+        for (PersonalInformation pi : infoCollection) {
+            System.out.print(pi.getFirstName() + " " + pi.getLastName() + "\n");
+        }
     }
 }
