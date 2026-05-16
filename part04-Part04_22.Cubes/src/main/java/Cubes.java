9395e4fcf5eec58caabbc247bd542791f8c17028
Harry <hcoyle1992@gmail.com>
2026-05-17 00:25:33 +0100

    more exercises

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,18 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            int number = Integer.parseInt(input);
+
+            //cast back to int for unit test
+            System.out.println((int)Math.pow(number, 3));
+
+        }
     }
 }
