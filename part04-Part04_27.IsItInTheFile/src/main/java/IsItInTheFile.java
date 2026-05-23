6d70411e6f45f0f29908c282b1cb5c939dca1b3c
Harry <hcoyle1992@gmail.com>
2026-05-23 12:24:53 +0100

    Add file search: - ask user for name of file to be read - prompt them to enter a name to search for - read file and search for specified string - handle file read errors - print "Found" or "Not found"

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,5 +1,6 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IsItInTheFile {
@@ -10,8 +11,51 @@ public class IsItInTheFile {
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
 
-        System.out.println("Search for:");
-        String searchedFor = scanner.nextLine();
+        ArrayList<String> list = readFile(file);
 
+        if (list == null) {
+            System.out.println("Reading the file " + file + " failed.");
+            return;
+        }
+
+        System.out.println();
+
+
+        while (true) {
+            System.out.println("Search for:");
+
+            // if no more lines in file, break
+            if (!scanner.hasNextLine()) {
+                break;
+            }
+
+            String searchedFor = scanner.nextLine();
+
+            // if user enters empty string, break
+            if (searchedFor.isEmpty()) {
+                break;
+            }
+
+            if (list.contains(searchedFor)) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static ArrayList<String> readFile(String file) {
+        ArrayList<String> list = new ArrayList<>();
+
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                list.add(fileScanner.nextLine());
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return list;
     }
 }
