30c58ad7f0b244509ab8cb1454db076ecce92d9e
Harry <hcoyle1992@gmail.com>
2026-06-13 08:32:02 +0100

    - prompts user for filename to search and lower/upper bound numbers - returns if no file exists w/ name - reads file contents to an ArrayList - iterates over ArrayList and counts numbers within provided bounds - prints count of numbers within bounds in the file

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,5 +1,9 @@
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
@@ -10,10 +14,50 @@ public class NumbersFromAFile {
         System.out.print("File? ");
         String file = scanner.nextLine();
         System.out.print("Lower bound? ");
-        int lowerBound = Integer.valueOf(scanner.nextLine());
+        int lowerBound = Integer.parseInt(scanner.nextLine());
         System.out.print("Upper bound? ");
-        int upperBound = Integer.valueOf(scanner.nextLine());
+        int upperBound = Integer.parseInt(scanner.nextLine());
+
+        //check that the file exists
+        Path path = Paths.get(file);
+
+        if (!Files.exists(path)) {
+            System.out.println("A file with that name doesn't exist.");
+            return;
+        }
+
+        ArrayList<String> listOfNums = readFileContents(file);
+
+        int numberCount = 0;
+
+        for (String number : listOfNums) {
+            if (Integer.parseInt(number) >= lowerBound && Integer.parseInt(number) <= upperBound) {
+                numberCount ++;
+            }
+        }
+
+        System.out.println("Numbers: " + numberCount);
+
 
     }
 
+    public static ArrayList<String> readFileContents(String fileName) {
+        ArrayList<String> fileContents = new ArrayList<>();
+
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                fileContents.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return fileContents;
+    }
 }
